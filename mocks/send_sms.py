22a3dee5a84334b409d5b8d60c9ef9dfac54d740
lekstonjm <lekston.jm@gmail.com>
2019-08-02 19:10:01 +0200

    add text format

--- mocks/send_sms.py
+++ mocks/send_sms.py
@@ -9,6 +9,12 @@ time.sleep(1)
 if s.inWaiting():
     print(s.read_all().decode('utf_8'))
 
+s.write(b'AT+CMGF=1\r\n')
+s.flush()
+time.sleep(1)
+if s.inWaiting():
+    print(s.read_all().decode('utf_8'))
+
 s.write(b'AT+CMGS="+33649122408"\r\n')
 s.flush()
 time.sleep(1)
