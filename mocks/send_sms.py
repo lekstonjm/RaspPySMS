56cd393b9c06665bef5c8d080ce0d9494ff699fc
lekstonjm <lekston.jm@gmail.com>
2019-08-02 19:06:37 +0200

    split text from phone number

--- mocks/send_sms.py
+++ mocks/send_sms.py
@@ -2,13 +2,21 @@ import serial
 import time
 
 s = serial.Serial("/dev/ttyS0", 115200)
+
 s.write(b'AT\r\n')
 s.flush()
 time.sleep(1)
 if s.inWaiting():
     print(s.read_all().decode('utf_8'))
-s.write(b'AT+CMGS="+33649122408"\r\ntext\x1a\r\n')
-time.sleep(1)
+
+s.write(b'AT+CMGS="+33649122408"\r\n')
 s.flush()
+time.sleep(1)
+if s.inWaiting():
+    print(s.read_all().decode('utf_8'))
+
+s.write(b'text\x1a\r\n')
+s.flush()
+time.sleep(1)
 if s.inWaiting():
     print(s.read_all().decode('utf_8'))
