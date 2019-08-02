b19f285d8ca97171099c24c982c44e11e0ebfd6a
lekston <lekston.jm@gmail.com>
2019-08-02 18:50:10 +0200

    correction

--- mocks/send_sms.py
+++ mocks/send_sms.py
@@ -1,14 +1,10 @@
 import serial
 import time
 
-def read(s):
-    data = ""
-    while s.inWaiting():
-        data += s.read_all()
-    return data
 s = serial.Serial("/dev/ttyS0", 115200)
 s.write(b'AT\r\n')
 s.flush()
 time.sleep(1)
-print(data)
+if s.inWaiting():
+    print(s.read_all().decode('utf_8'))
 
