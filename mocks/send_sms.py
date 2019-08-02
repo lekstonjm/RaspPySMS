6e8b09e22e9cad7b6380a082c473b429b0ef0271
lekstonjm <lekston.jm@gmail.com>
2019-08-02 18:31:04 +0200

    first test

---
+++ mocks/send_sms.py
@@ -0,0 +1,14 @@
+import serial
+import time
+
+def read(s):
+    data = ""
+    while s.inWaiting():
+        data += s.read_all()
+    return data
+s = serial.Serial("/dev/ttyS0", 115200)
+s.write(b'AT\r\n')
+s.flush()
+time.sleep(1)
+print(data)
+
