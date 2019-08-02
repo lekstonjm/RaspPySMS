ff7a9c6b5b0093b5d9c24194354ae397ad7e1ea5
lekstonjm <lekston.jm@gmail.com>
2019-08-02 18:58:46 +0200

    quick and dirty sent

--- mocks/send_sms.py
+++ mocks/send_sms.py
@@ -7,4 +7,7 @@ s.flush()
 time.sleep(1)
 if s.inWaiting():
     print(s.read_all().decode('utf_8'))
-
+s.write(b'AT+CMGS="+33649122408"\r\ntext\x1a\r\n')
+time.sleep(1)
+if s.inWaiting():
+    print(s.read_all().decode('utf_8'))
